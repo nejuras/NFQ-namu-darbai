135c609ee1acf8e4d7a84c04e7d0c2b678d9146e
nejuras <nerusha@gmail.com>
2018-04-27 11:40:05 +0300

    update

--- src/App/Controller/WeatherController.php
+++ src/App/Controller/WeatherController.php
@@ -28,8 +28,6 @@ class WeatherController extends Controller
      */
     public function index(): Response
     {
-        $markdownTransformer = $this->get('nfq_weather.provider.openweathermap');
-        var_dump($markdownTransformer);
         $location = new Location(54.6872, 25.2797);
         $weather = $this->provider->fetch($location);
         return $this->render('home/index.html.twig', [
